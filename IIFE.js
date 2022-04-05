275ca08c6030d99161b2968fda9f83f915aa3f5e
tuduan1902 <uuxinhxinh2701@gmail.com>
2022-04-05 22:32:28 +0700

    commit

---
+++ IIFE.js
@@ -0,0 +1,12 @@
+// IIFE - Immediately Invoked Function Expression
+// Define: Một function expression được gọi ngay lập tức
+// Nội dung
+// 1. IIFE trông như nào?
+    (function(){
+        console.log('NOW NOW')
+    })()
+// 2. Dùng dấu ; trước IIFE
+// 3. IIFE là hàm "private"
+// 4. Sử dụng IIFE khi nào?
+// 5. Các cách tạo ra một IIFE 
+// 6. Ví dụ sử dụng IIFE
