a1975128805b1534dee5ebd84c15a369e8742df7
tuduan1902 <uuxinhxinh2701@gmail.com>
2022-04-06 09:07:02 +0700

    commit

--- IIFE.js
+++ IIFE.js
@@ -6,7 +6,33 @@
         console.log('NOW NOW')
     })()
 // 2. Dùng dấu ; trước IIFE
+    // Dành cho project không sử dụng dấu ; ở cuối code
 // 3. IIFE là hàm "private"
+    (function myFunc(){
+        console.log('Now');
+    })()
+    myFunc() // không chạy được
 // 4. Sử dụng IIFE khi nào?
+    // Thư viện thường viết bằng IIFE để tránh ảnh hưởng đến dự án của người sử dụng thư viện
+    // Thể hiện được tính an toàn dữ liệu 
 // 5. Các cách tạo ra một IIFE 
 // 6. Ví dụ sử dụng IIFE
+    const app = (function(){
+        // Private
+        const cars = []
+        return {
+            get(index) {
+                return cars[index]
+            },
+            add(car) {
+                cars.push(car)
+            },
+            edit(index, car) {
+                cars[index] = car
+            },
+            delete(index) {
+                cars.splice(index, 1)
+            }
+        }
+    })
+
