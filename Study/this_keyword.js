44760f914639ee65999865a2905df49deb7a68d9
Tu Duan <uuxinhxinh2701@gmail.com>
2022-04-08 23:04:00 +0700

    commit

--- Study/this_keyword.js
+++ Study/this_keyword.js
@@ -1,16 +1,33 @@
-// Từ khóa this trỏ về đối tượng đang thuộc về
-const iphone7 = {
-    // Thuộc tính - Property
-    name:  'iPhone  7',
-    color: 'Pink',
-    weight: 300,
-    //  Phương thức - Method
-    takePhoto() {
-        console.log('Take a photo');
-    },
-    objChild: {
-        methodChild() {
-            console
-        }
+// Từ khóa this trong Javasript đề cập đến đối tượng mà nó thuộc về 
+// Đặc Tính
+// 1. Trong một phương thức, this tham chiếu tới đối tượng truy cập method(trước dấu chấm)
+// const iphone7 = {
+//     // Thuộc tính - Property
+//     name:  'iPhone  7',
+//     color: 'Pink',
+//     weight: 300,
+//     //  Phương thức - Method
+//     takePhoto() {
+//         console.log('Take a photo');
+//     },
+//     objChild: {
+//         name:'Object child',
+//         methodChild() {
+//             console.log(this)
+//         }
+//     }
+// }
+// iphone7.objChild.methodChild()
+
+// 2. Đứng ngoài method, this tham chiếu tới đối tượng global
+// Lưu ý 
+// this trong hàm tạo là đại diện cho đối tượng được tạo
+    function Car(name, color) {
+        this.name = name
+        this.color = color
     }
-}
+    const porsche = new Car('Porsche','black') // this chính la porsche trong TH này
+// this trong một hàm là undefined khi ở strict mode
+// Các method bind(), call(), apply() có thểtham chiếu this tới đối  tượng khác
+
+
