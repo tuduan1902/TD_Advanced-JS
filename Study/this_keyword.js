775e87f6e1602d3a1710d5204cc30ea7d02e7a5c
Tu Duan <uuxinhxinh2701@gmail.com>
2022-04-08 22:37:25 +0700

    commit

---
+++ Study/this_keyword.js
@@ -0,0 +1,16 @@
+// Từ khóa this trỏ về đối tượng đang thuộc về
+const iphone7 = {
+    // Thuộc tính - Property
+    name:  'iPhone  7',
+    color: 'Pink',
+    weight: 300,
+    //  Phương thức - Method
+    takePhoto() {
+        console.log('Take a photo');
+    },
+    objChild: {
+        methodChild() {
+            console
+        }
+    }
+}
