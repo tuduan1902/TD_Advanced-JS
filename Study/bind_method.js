c98f9542ef6bb7514593add71e7e73df7c8b875e
Tu Duan <uuxinhxinh2701@gmail.com>
2022-04-10 14:31:32 +0700

    commit

--- Study/bind_method.js
+++ Study/bind_method.js
@@ -21,4 +21,51 @@ console.log(getTeacherName()) // "Minh Thu" , TH này gtTeacherName là function
 const getTeacherName1 = teacher.getFullName.bind(teacher)
 console.log(getTeacherName1()) // "Minh Thảo" , teacher ràng buộc với từ khóa this trả ra nên khi call 
 // // getTeacherName1 thì this sẽ là teacher( đối số truyền vào bind() )
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const app = (() => {
+    const  cars = ['BMW']
+    const root = $('#root')
+    const input = $('#input')
+    const submit = $('#submit')
+
+    return {
+        add(car) {
+            cars.push(car)
+        },
+        delete(index) {
+            cars.splice(index, 1)
+        },
+        render() {
+            const html = cars.map((car,index) => `
+                <li>
+                    ${car}
+                    <span class="delete" data-index="${index}">&times</span>
+                </li>
+            `)
+            .join('')
+
+            root.innerHTML = html
+        },
+        handleDelete(event) {
+            
+        },
+        init() {
+            // Handle Dom events
+            submit.onclick = () => {
+                const car = input.value
+                this.add(car)
+                this.render()
+                input.value = null
+                input.focus()
+            }
+            root.onclick =  
+            this.render()
+        }
+
+    }
+})();
+app.init()
+
 
--- Study/this_keyword.js
+++ Study/this_keyword.js
@@ -1,57 +1,32 @@
 // Từ khóa this trong Javasript đề cập đến đối tượng mà nó thuộc về 
 // Đặc Tính
 // 1. Trong một phương thức, this tham chiếu tới đối tượng truy cập method(trước dấu chấm)
-// const iphone7 = {
-//     // Thuộc tính - Property
-//     name:  'iPhone  7',
-//     color: 'Pink',
-//     weight: 300,
-//     //  Phương thức - Method
-//     takePhoto() {
-//         console.log('Take a photo');
-//     },
-//     objChild: {
-//         name:'Object child',
-//         methodChild() {
-//             console.log(this)
-//         }
-//     }
-// }
-// iphone7.objChild.methodChild()
+const iphone7 = {
+    // Thuộc tính - Property
+    name:  'iPhone  7',
+    color: 'Pink',
+    weight: 300,
+    //  Phương thức - Method
+    takePhoto() {
+        console.log('Take a photo');
+    },
+    objChild: {
+        name:'Object child',
+        methodChild() {
+            console.log(this)
+        }
+    }
+}
+iphone7.objChild.methodChild()
 
 // 2. Đứng ngoài method, this tham chiếu tới đối tượng global
 // Lưu ý 
-// this trong hàm tạo là đại diện cho đối tượng được tạo
-    // function Car(name, color) {
-    //     this.name = name
-    //     this.color = color
-    // }
-    // const porsche = new Car('Porsche','black') // this chính la porsche trong TH này
+//this trong hàm tạo là đại diện cho đối tượng được tạo
+    function Car(name, color) {
+        this.name = name
+        this.color = color
+    }
+    const porsche = new Car('Porsche','black') // this chính la porsche trong TH này
 // this trong một hàm là undefined khi ở strict mode
 // Các method bind(), call(), apply() có thểtham chiếu this tới đối  tượng khác
 
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-
-const app = (() => {
-    const  cars = ['BMW']
-    const root = $('#root')
-
-    return {
-        add(car) {
-            car.push(car)
-        },
-        delete(index) {
-            cars.splice(index, 1)
-        },
-        render() {
-            const html = cars.map(car => `
-                <li>${car}</li>
-            `)
-            .join('')
-
-            root.innerHTML = html
-        }
-
-    }
-})
